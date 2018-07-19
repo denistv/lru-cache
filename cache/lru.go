1fa46a20b0cf0fa21c755ce6613fb5e503c69b0a
Denis Tv <denistv777@gmail.com>
2018-07-20 02:47:27 +0300

    Init

---
+++ cache/lru.go
@@ -0,0 +1,58 @@
+package cache
+
+import (
+	"container/list"
+	"errors"
+)
+
+const ElementNotFound = -1
+
+func NewLRU(capacity int) (LRU, error) {
+	if capacity <= 0 {
+		return LRU{}, errors.New("capacity must be gt 0")
+	}
+
+	return LRU{
+		capacity: capacity,
+		list:     list.New(),
+		elements: make(map[int]*list.Element),
+	}, nil
+}
+
+type LRU struct {
+	capacity int
+	list     *list.List
+	elements map[int]*list.Element
+}
+
+func (l *LRU) Put(key int, value int) {
+	element := l.elements[key]
+
+	if element == nil {
+		element = l.list.PushFront(key)
+		l.elements[key] = element
+	}
+
+	if l.list.Len() > l.capacity {
+		l.removeOldest()
+	}
+}
+
+func (l *LRU) Get(key int) int {
+	if element, ok := l.elements[key]; ok {
+		l.list.MoveToFront(element)
+
+		return element.Value.(int)
+	}
+
+	return ElementNotFound
+}
+
+func (l *LRU) removeOldest() {
+	back := l.list.Back()
+
+	if back != nil {
+		l.list.Remove(back)
+		delete(l.elements, back.Value.(int))
+	}
+}
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"lru/cache"
+)
+
+func main() {
+	lru, err := cache.NewLRU(2)
+
+	if err != nil {
+		panic(err)
+	}
+
+	lru.Put(1, 1)
+	lru.Put(2, 2)
+	fmt.Println(lru.Get(1))
+	lru.Put(3, 3)
+	fmt.Println(lru.Get(2))
+	lru.Put(4, 4)
+	fmt.Println(lru.Get(1))
+	fmt.Println(lru.Get(3))
+	fmt.Println(lru.Get(4))
+}
